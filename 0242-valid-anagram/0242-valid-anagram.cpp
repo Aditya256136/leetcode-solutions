cbdb351191c88e2bbdd9a601764fb479bb9ba524
Rishi <69592864+Aditya256136@users.noreply.github.com>
2026-07-29 04:59:54 -0400

    Time: 0 ms (100%), Space: 9.9 MB (11.58%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length())
+            return false;
+        unordered_map<char, int> mp;
+        for(char i : s)
+            mp[i]++;
+        for(char i : t)
+        {
+            mp[i]--;
+            if(mp[i] < 0)
+                return false;
+        }
+        return true;
+    }
+};
